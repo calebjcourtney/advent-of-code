a3b3394dee4b4368aa4ef854e284b1e6dc913ad5
calebjcourtney <brokenfoot@rocketmail.com>
2020-12-01 21:18:50 -0800

    adding code for day 2

---
+++ 2020/day02.py
@@ -0,0 +1,44 @@
+def parse_line(record):
+    r, letter, password = record.split()
+    letter = letter[0]
+
+    start, end = r.split("-")
+    start = int(start)
+    end = int(end)
+
+    return start, end, letter, password
+
+
+def part_1(data):
+    valid = 0
+    for record in data:
+        if len(record) < 1:
+            continue
+
+        start, end, letter, password = parse_line(record)
+
+        if password.count(letter) >= start and password.count(letter) <= end:
+            valid += 1
+
+    return valid
+
+
+def part_2(data):
+    valid = 0
+    for record in data:
+        if len(record) < 1:
+            continue
+
+        start, end, letter, password = parse_line(record)
+
+        if (password[start - 1] == letter) ^ (password[end - 1] == letter):
+            valid += 1
+
+    return valid
+
+
+if __name__ == '__main__':
+    data = open("input02.txt", "r").read().split("\n")
+
+    print(part_1(data))
+    print(part_2(data))
