3fc1819d432c21cbb7c9fd332b6eb4ad5799f159
calebjcourtney <brokenfoot@rocketmail.com>
2020-12-02 22:01:19 -0800

    clean up code

--- 2020/python/day03.py
+++ 2020/python/day03.py
@@ -1,35 +1,18 @@
 from utils import get_line_data
 
+from numpy import prod
 
-data = get_line_data("03")
 
-
-def part_one(data):
+def get_trees(data, right, down):
     trees = 0
     for x in range(len(data)):
-        if data[x][(x * 3) % len(data[0])] == "#":
+        if (x * down <= len(data)) and (data[x * down][(x * right) % len(data[0])] == "#"):
             trees += 1
-
-    return trees
-
-
-def part_two(data):
-    trees = 1
-    for right, down in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]:
-        temp = 0
-        for x in range(len(data)):
-            try:
-                if data[x * down][(x * right) % len(data[0])] == "#":
-                    temp += 1
-            except IndexError:
-                break
-
-        trees *= temp
 
     return trees
 
 
 if __name__ == '__main__':
     data = get_line_data("03")
-    print(part_one(data))
-    print(part_two(data))
+    print(get_trees(data, 3, 1))
+    print(prod([get_trees(data, right, down) for right, down in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]]))
