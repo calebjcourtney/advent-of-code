c7a5e10afbfd2eca271d9afbbcbe3b70d5d98fe2
calebjcourtney <brokenfoot@rocketmail.com>
2020-12-04 07:28:02 -0800

    updating requirements to be lambda functions (stolen from Peter)

--- 2020/python/day04.py
+++ 2020/python/day04.py
@@ -15,58 +15,27 @@ def is_valid_1(passport):
         return True
 
 
+TERM_PARAMS = {
+    "byr": lambda x: int(x) in range(1920, 2003),
+    "iyr": lambda x: int(x) in range(2010, 2021),
+    "eyr": lambda x: int(x) in range(2020, 2031),
+    "hgt": lambda x: ("in" == x[-2:] and int(x[:-2]) in range(59, 77)) or ("cm" == x[-2:] and int(x[:-2]) in range(150, 194)),
+    "hcl": lambda x: x[0] == "#" and len(x) == 7 and int(x[1:], 16) >= 0,
+    "ecl": lambda x: x in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'],
+    "pid": lambda x: len(x) == 9 and int(x) >= 0,
+    "cid": lambda x: True
+}
+
+
 def is_valid_2(passport):
     terms = {}
     for term in passport.split():
         terms[term.split(':')[0].strip()] = term.split(':')[1].strip()
 
     try:
-        # years
-        terms['byr'] = int(terms['byr'])
-        if terms['byr'] not in range(1920, 2003):
-            return False
-        terms['iyr'] = int(terms['iyr'])
-        if terms['iyr'] not in range(2010, 2021):
-            return False
-        terms['eyr'] = int(terms['eyr'])
-        if terms['eyr'] not in range(2020, 2031):
-            return False
-
-        # height
-        terms['hgt_num'] = float(terms['hgt'][:-2])
-        terms['hgt_unit'] = terms['hgt'][-2:]
-
-        if terms['hgt_unit'] == 'cm':
-            if terms['hgt_num'] not in range(150, 194):
+        for term, value in terms.items():
+            if not TERM_PARAMS[term](value):
                 return False
-
-        elif terms['hgt_unit'] == 'in':
-            if terms['hgt_num'] not in range(59, 77):
-                return False
-
-        else:
-            return False
-
-        # hair and eye colour
-        if terms['hcl'][0] != '#':
-            return False
-
-        elif len(terms['hcl']) != 7:
-            return False
-
-        else:
-            # hex baby
-            int(terms['hcl'][1:], 16)
-
-        if terms['ecl'] not in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
-            return False
-
-        # passport ID
-        if len(terms['pid']) != 9:
-            return False
-        else:
-            # even if leading zeros, this should be okay
-            int(terms['pid'])
 
     except ValueError:
         return False
@@ -75,7 +44,6 @@ def is_valid_2(passport):
 
 
 data = get_data("04")
-
 passports = data.strip().split('\n\n')
 
 
