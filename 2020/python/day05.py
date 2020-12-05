aa5d9ddcbbf351fb7ebde346845d481c5ddfe387
calebjcourtney <brokenfoot@rocketmail.com>
2020-12-05 07:19:39 -0800

    adding in the cool translate functionality

--- 2020/python/day05.py
+++ 2020/python/day05.py
@@ -3,8 +3,10 @@ from utils import get_line_data
 
 data = get_line_data("05")
 
-
-seat_ids = [int(line.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1"), 2) for line in data]
+# convert to binary string
+t = str.maketrans({'B': '1', 'R': '1', 'F': '0', 'L': '0'})
+# then int convert to int
+seat_ids = [int(line.translate(t), 2) for line in data]
 
 
 # part 1
