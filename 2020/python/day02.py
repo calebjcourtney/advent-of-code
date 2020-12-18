204cafc36e399dd6a2932bccf0c0179559413ad2
calebjcourtney <brokenfoot@rocketmail.com>
2020-12-18 11:51:21 -0800

    added typing and condensed code

--- 2020/python/day02.py
+++ 2020/python/day02.py
@@ -1,7 +1,10 @@
 from utils import get_line_data
 
 
-def parse_line(record):
+from typing import Tuple
+
+
+def parse_line(record: str) -> Tuple[int, int, str, str]:
     r, letter, password = record.split()
     letter = letter[0]
 
@@ -12,36 +15,32 @@ def parse_line(record):
     return start, end, letter, password
 
 
-def part_1(data):
-    valid = 0
-    for record in data:
-        if len(record) < 1:
-            continue
+def part_1(record: str) -> int:
+    if len(record) < 1:
+        return 0
 
-        start, end, letter, password = parse_line(record)
+    start, end, letter, password = parse_line(record)
 
-        if password.count(letter) >= start and password.count(letter) <= end:
-            valid += 1
+    if password.count(letter) >= start and password.count(letter) <= end:
+        return 1
 
-    return valid
+    return 0
 
 
-def part_2(data):
-    valid = 0
-    for record in data:
-        if len(record) < 1:
-            continue
+def part_2(record: str) -> int:
+    if len(record) < 1:
+        return 0
 
-        start, end, letter, password = parse_line(record)
+    start, end, letter, password = parse_line(record)
 
-        if (password[start - 1] == letter) ^ (password[end - 1] == letter):
-            valid += 1
+    if (password[start - 1] == letter) ^ (password[end - 1] == letter):
+        return 1
 
-    return valid
+    return 0
 
 
 if __name__ == '__main__':
     data = get_line_data("02")
 
-    print(part_1(data))
-    print(part_2(data))
+    print(sum(map(part_1, data)))
+    print(sum(map(part_2, data)))
