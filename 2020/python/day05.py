5c5c12a7b25b54cb4b0378d3f9ddc2f9eb32b33c
calebjcourtney <brokenfoot@rocketmail.com>
2020-12-18 11:39:04 -0800

    clean up day 5 code and make consistent with other days

--- 2020/python/day05.py
+++ 2020/python/day05.py
@@ -1,19 +1,21 @@
 from utils import get_line_data
 
 
-data = get_line_data("05")
-
-# convert to binary string
-t = str.maketrans({'B': '1', 'R': '1', 'F': '0', 'L': '0'})
-# then convert to int
-seat_ids = [int(line.translate(t), 2) for line in data]
+def part_one(seat_ids):
+    return max(seat_ids)
 
 
-# part 1
-print(max(seat_ids))
+def part_two(seat_ids):
+    for x in range(min(seat_ids), max(seat_ids)):
+        if x not in seat_ids:
+            return x
 
 
-# part 2
-for x in range(min(seat_ids), max(seat_ids)):
-    if x not in seat_ids:
-        print(x)
+if __name__ == '__main__':
+    data = get_line_data("05")
+
+    t = str.maketrans({'B': '1', 'R': '1', 'F': '0', 'L': '0'})
+    seat_ids = [int(line.translate(t), 2) for line in data]
+
+    print(part_one(seat_ids))
+    print(part_two(seat_ids))
