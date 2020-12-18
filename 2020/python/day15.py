547930e2b2c205296e5599059bb617d84753f98f
calebjcourtney <brokenfoot@rocketmail.com>
2020-12-18 09:33:41 -0800

    clean up day 15

--- 2020/python/day15.py
+++ 2020/python/day15.py
@@ -1,20 +1,20 @@
-data = "14,3,1,0,9,5"
-data = list(map(int, data.split(',')))
+from utils import get_data
 
-p1, p2 = 0, 0
 
-last = {v: i for i, v in enumerate(data, 1)}
-prev = data[-1]
-times = 30000000
-for i in range(len(data) + 1, times + 1):
-    new = i - 1 - last[prev] if prev in last else 0
-    last[prev] = i - 1
-    prev = new
+def solve(data, limit):
+    last = {v: i for i, v in enumerate(data, 1)}
+    prev = data[-1]
+    for i in range(len(data) + 1, limit + 1):
+        new = i - 1 - last[prev] if prev in last else 0
+        last[prev] = i - 1
+        prev = new
 
-    if i == 2020:
-        p1 = prev
+    return prev
 
-p2 = prev
 
-print(f'Part 1: {p1}')
-print(f'Part 2: {p2}')
+if __name__ == '__main__':
+    data = get_data("15")
+    data = list(map(int, data.split(',')))
+
+    print(solve(data, 2020))
+    print(solve(data, 30000000))
