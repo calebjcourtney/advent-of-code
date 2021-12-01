4589d286bd0e6c3ae2d7c25a88ed219a0950ed21
calebjcourtney <brokenfoot@rocketmail.com>
2021-11-30 21:48:47 -0800

    updating to use windowed, which is way cooler

--- 2021/python/day01.py
+++ 2021/python/day01.py
@@ -1,12 +1,14 @@
 from typing import List
 
+from more_itertools import windowed
+
 from utils import get_int_data
 
 
 def part_one(data: List[int]) -> int:
     count = 0
-    for x, y in zip(data, data[1:]):
-        if y > x:
+    for x, y in windowed(data, 2):
+        if x < y:
             count += 1
 
     return count
@@ -14,8 +16,8 @@ def part_one(data: List[int]) -> int:
 
 def part_two(data: List[int]) -> int:
     count = 0
-    first = data[0], data[1], data[2]
-    for second in zip(data[1:], data[2:], data[3:]):
+    first = data[:2]
+    for second in windowed(data[1:], 3):
         if sum(first) < sum(second):
             count += 1
 
