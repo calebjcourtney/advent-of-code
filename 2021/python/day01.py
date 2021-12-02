771eec58a30ef0cf2cd0f591da83e7fab69d509a
calebjcourtney <brokenfoot@rocketmail.com>
2021-12-01 16:13:49 -0800

    this is a cleaner solution

--- 2021/python/day01.py
+++ 2021/python/day01.py
@@ -1,17 +1,8 @@
-from typing import List
-
-from utils import get_int_data, nwise
-
-
-def count_increasing(num_list: List):
-    return sum(x < y for x, y in nwise(num_list, 2))
+from utils import get_int_data
 
 
 if __name__ == '__main__':
     data = get_int_data("01")
 
-    p1_result = count_increasing(data)
-    print(p1_result)
-
-    p2_result = count_increasing([sum(x) for x in nwise(data, 3)])
-    print(p2_result)
+    print(sum(x < y for x, y in zip(data, data[1:])))
+    print(sum(x < y for x, y in zip(data, data[3:])))
