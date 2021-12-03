9a433ab5553428ce1f28ce72027fa49a8fda93ce
calebjcourtney <brokenfoot@rocketmail.com>
2021-12-03 07:08:38 -0800

    cleaning up day 3 more

--- 2021/python/day03.py
+++ 2021/python/day03.py
@@ -1,20 +1,18 @@
 from utils import get_line_data
 
+from collections import Counter
 
-def most_common(data, column, flip):
-    oxygen = 0
-    co2 = 0
 
-    for row in data:
-        if row[column] == "1":
-            oxygen += 1
-        else:
-            co2 += 1
+def most_common(data, index, invert):
+    c = Counter(row[index] for row in data)
+
+    oxygen = c["1"]
+    co2 = c["0"]
 
     if oxygen == co2:
-        return "0" if flip else "1"
+        return "0" if invert else "1"
 
-    if flip:
+    if invert:
         oxygen, co2 = co2, oxygen
 
     return "1" if oxygen >= co2 else "0"
@@ -33,28 +31,23 @@ def part_one(data):
     return int(gamma, 2) * int(epsilon, 2)
 
 
+def filter_data(data, invert):
+    i = 0
+    while len(data) != 1:
+        common_val = most_common(data, i, invert)
+        data = [line for line in data if line[i] == common_val]
+        i += 1
+
+    val = data[0]
+
+    return val
+
+
 def part_two(data):
-    # oxygen
-    tmp = data[:]
-    i = 0
-    while len(tmp) != 1:
-        common_val = most_common(tmp, i, False)
-        tmp = [line for line in tmp if line[i] == common_val]
-        i += 1
+    oxygen = filter_data(data, False)
+    co2 = filter_data(data, True)
 
-    oxy = tmp[0]
-
-    # co2
-    tmp = data[:]
-    i = 0
-    while len(tmp) != 1:
-        common_val = most_common(tmp, i, True)
-        tmp = [line for line in tmp if line[i] == common_val]
-        i += 1
-
-    co2 = tmp[0]
-
-    return int(oxy, 2) * int(co2, 2)
+    return int(oxygen, 2) * int(co2, 2)
 
 
 if __name__ == '__main__':
