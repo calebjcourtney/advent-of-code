90e1f0b3f572481cfb6213e46ab8f12a92a73794
calebjcourtney <brokenfoot@rocketmail.com>
2021-12-04 07:52:06 -0800

    adding documentation and typing

--- 2021/python/day04.py
+++ 2021/python/day04.py
@@ -2,32 +2,40 @@ from utils import get_data
 
 
 class Board:
-    def __init__(self, data):
+    def __init__(self, data: str):
+        # initialize the data structures
+        # data are the numbers in the board
+        # guesses are the numbers already guessed on the board
+
         self.data = []
         for row in data.split("\n"):
             self.data.append(list(map(int, row.split())))
 
         self.guesses = [[False for _ in range(5)] for _ in range(5)]
 
-    def has_number(self, num):
+    def has_number(self, num: int) -> [bool, None]:
+        # finds an input number in the board and sets the guessed list to True
         for x, row in enumerate(self.data):
             for y, column in enumerate(row):
                 if num == column:
                     self.guesses[x][y] = True
                     return True
 
-    def is_bingo(self):
+    def is_bingo(self) -> [bool, None]:
+        # checks if the board is a bingo (row or column complete)
+
+        # row is complete
         for row in self.guesses:
             if all(row):
                 return True
 
+        # column is complete
         for col_i in range(len(self.guesses)):
             if all([row[col_i] for row in self.guesses]):
                 return True
 
-        return False
-
-    def bingo_data(self):
+    def bingo_data(self) -> int:
+        # returns the sum of the numbers in the board not already guessed in the sequence
         c = 0
         for x, row in enumerate(self.guesses):
             for y, column in enumerate(row):
@@ -36,7 +44,7 @@ class Board:
 
         return c
 
-    def solve(self, sequence):
+    def solve(self, sequence) -> int:
         for i, number in enumerate(sequence):
             if self.has_number(number) and self.is_bingo():
                 return i, number * self.bingo_data()
@@ -45,17 +53,17 @@ class Board:
 if __name__ == '__main__':
     data = get_data("04")
 
+    # break out the inputs - nums vs boards
     numbers = list(map(int, data.split("\n\n")[0].split(",")))
+    boards = [Board(board) for board in data.split("\n\n")[1:]]
 
-    boards = []
-    for board in data.split("\n\n")[1:]:
-        b = Board(board)
-        boards.append(b)
-
+    # solve every board and sort by the number of iterations it takes for it to be solved
     solved_boards = sorted([board.solve(numbers) for board in boards], key = lambda board: board[0])
 
+    # this is the fastest solution
     p1_result = solved_boards[0][1]
     print(p1_result)
 
+    # this is the slowest solution
     p2_result = solved_boards[-1][1]
     print(p2_result)
