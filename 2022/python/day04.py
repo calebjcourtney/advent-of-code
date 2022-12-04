bd9095a0899f21112916cfc8629be920cdf36351
Caleb Courtney <caleb-courtney@pluralsight.com>
2022-12-03 21:15:38 -0800

    add code for day 4

---
+++ 2022/python/day04.py
@@ -0,0 +1,38 @@
+from utils import get_line_data
+
+
+class Section():
+    def __init__(self, arg):
+        self.start, self.end = tuple(map(int, arg.split("-")))
+        self.seats = set([x for x in range(self.start, self.end + 1)])
+
+
+def part_one(data):
+    c = 0
+    for line in data:
+        s1, s2 = map(Section, line.split(","))
+
+        if len(s1.seats & s2.seats) == min(len(s1.seats), len(s2.seats)):
+            c += 1
+
+    return c
+
+
+def part_two(data):
+    c = 0
+    for line in data:
+        s1, s2 = map(Section, line.split(","))
+        if len(s1.seats & s2.seats):
+            c += 1
+
+    return c
+
+
+if __name__ == '__main__':
+    data = get_line_data("04")
+
+    p1_result = part_one(data)
+    print(p1_result)
+
+    p2_result = part_two(data)
+    print(p2_result)
