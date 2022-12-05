36aaf9bfe7cba917c06a82d18889eb3603529290
Caleb Courtney <caleb-courtney@pluralsight.com>
2022-12-04 23:01:10 -0800

    slight code cleanup for day 5

--- 2022/python/day05.py
+++ 2022/python/day05.py
@@ -1,27 +1,15 @@
-from typing import Tuple
+from typing import Dict, List, Tuple
 import re
 
 from utils import get_data
 
 
-def parse_crate_from_line(line: str):
+def parse_crate_from_line(line: str) -> List[str]:
     results = re.findall(r'\[(\w|\s)\]', line)
     return [
         x.strip("[").strip("]")
         for x in results
     ]
-
-
-def build_stacks(stack_data):
-    stacks = {k: Stack([]) for k in range(1, 10)}
-    for line in stack_data.split("\n"):
-        crates = parse_crate_from_line(line)
-        for index, crate in enumerate(crates, 1):
-            if crate == " ":
-                continue
-            stacks[index].insert(crate)
-
-    return stacks
 
 
 def parse_instructions(line: str) -> Tuple[int, int, int]:
@@ -48,10 +36,20 @@ class Stack(object):
         return self.crates[-1]
 
 
-def part_one(stacks, instructions):
-    for line in instructions.split("\n"):
-        count, source, target = parse_instructions(line)
+def build_stacks(stack_data) -> Dict[int, Stack]:
+    stacks = {k: Stack([]) for k in range(1, 10)}
+    for line in stack_data.split("\n"):
+        crates = parse_crate_from_line(line)
+        for index, crate in enumerate(crates, 1):
+            if crate == " ":
+                continue
+            stacks[index].insert(crate)
 
+    return stacks
+
+
+def part_one(stacks: Dict[int, Stack], instructions: str) -> str:
+    for (count, source, target) in instructions:
         crates = stacks[source].drop(count)
         crates = crates[::-1]
         stacks[target].add(crates)
@@ -59,10 +57,8 @@ def part_one(stacks, instructions):
     return "".join(stacks[i].top() for i in range(1, len(stacks) + 1))
 
 
-def part_two(stacks, instructions):
-    for line in instructions.split("\n"):
-        count, source, target = parse_instructions(line)
-
+def part_two(stacks: Dict[int, Stack], instructions: Tuple[int, int, int]) -> str:
+    for (count, source, target) in instructions:
         crates = stacks[source].drop(count)
         stacks[target].add(crates)
 
@@ -73,6 +69,7 @@ if __name__ == '__main__':
     data = get_data("05")
 
     stack_data, instructions = data.split("\n\n")
+    instructions = [parse_instructions(line) for line in instructions.split("\n")]
     stacks = build_stacks(stack_data)
 
     p1_result = part_one(stacks, instructions)
