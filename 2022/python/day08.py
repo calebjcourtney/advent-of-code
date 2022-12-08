a2020d819843340911a26eba8a0819f87098e182
Caleb Courtney <caleb-courtney@pluralsight.com>
2022-12-08 08:26:18 -0800

    code cleanup

--- 2022/python/day08.py
+++ 2022/python/day08.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from utils import get_line_data
 
 import numpy as np
@@ -9,26 +7,26 @@ class Grid:
     def __init__(self, data):
         self.data = data
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "\n".join([" ".join(map(str, row)) for row in self.data])
 
-    def north_visible(self, x, y):
+    def north_visible(self, x: int, y: int) -> bool:
         r = [self.data[yi][x] for yi in range(y)]
         return True if not r else self.data[y][x] > max(r)
 
-    def south_visible(self, x, y):
+    def south_visible(self, x: int, y: int) -> bool:
         r = [self.data[yi][x] for yi in range(y + 1, len(self.data))]
         return True if not r else self.data[y][x] > max(r)
 
-    def west_visible(self, x, y):
+    def west_visible(self, x: int, y: int) -> bool:
         r = [self.data[y][xi] for xi in range(x)]
         return True if not r else self.data[y][x] > max(r)
 
-    def east_visible(self, x, y):
+    def east_visible(self, x: int, y: int) -> bool:
         r = [self.data[y][xi] for xi in range(x + 1, len(self.data[0]))]
         return True if not r else self.data[y][x] > max(r)
 
-    def is_visible(self, x, y):
+    def is_visible(self, x: int, y: int) -> bool:
         return any([
             self.north_visible(x, y),
             self.south_visible(x, y),
@@ -36,69 +34,50 @@ class Grid:
             self.east_visible(x, y),
         ])
 
-    def visible_trees(self):
-        total = 0
-        for y in range(len(self.data)):
-            for x in range(len(self.data[0])):
-                if self.is_visible(x, y):
-                    total += 1
+    def visible_trees(self) -> int:
+        return sum([
+            self.is_visible(x, y)
+            for y in range(len(self.data))
+            for x in range(len(self.data[y]))
+        ])
 
-        return total
-
-    def north_count(self, x, y):
-        if y == 0:
-            return 0
-
+    def north_count(self, x: int, y: int) -> int:
         count = 0
         for yi in range(y - 1, -1, -1):
             count += 1
             if self.data[yi][x] >= self.data[y][x]:
                 break
 
-        assert isinstance(count, int)
         return count
 
-    def south_count(self, x, y):
-        if y == len(data):
-            return 0
-
+    def south_count(self, x: int, y: int) -> int:
         count = 0
         for yi in range(y + 1, len(data)):
             count += 1
             if self.data[yi][x] >= self.data[y][x]:
                 break
 
-        assert isinstance(count, int)
         return count
 
-    def west_count(self, x, y):
-        if x == 0:
-            return 0
-
+    def west_count(self, x: int, y: int) -> int:
         count = 0
         for xi in range(x - 1, -1, -1):
             count += 1
             if self.data[y][xi] >= self.data[y][x]:
                 break
 
-        assert isinstance(count, int)
         return count
 
-    def east_count(self, x, y):
-        if x == len(data[y]):
-            return 0
-
+    def east_count(self, x: int, y: int) -> int:
         count = 0
         for xi in range(x + 1, len(data[y])):
             count += 1
             if self.data[y][xi] >= self.data[y][x]:
                 break
 
-        assert isinstance(count, int)
-
         return count
 
-    def scenic_score(self, x, y):
+    def scenic_score(self, x: int, y: int) -> int:
         return np.prod([
             self.north_count(x, y),
             self.south_count(x, y),
@@ -106,7 +85,7 @@ class Grid:
             self.west_count(x, y)
         ])
 
-    def best_scenic_score(self):
+    def best_scenic_score(self) -> int:
         top_score = 0
         for y in range(len(self.data)):
             for x in range(len(self.data[0])):
@@ -116,18 +95,9 @@ class Grid:
         return top_score
 
 
-def parse_data(data: List[str]) -> Grid:
-    grid = Grid([list(map(int, row)) for row in data])
-    for y in grid.data:
-        for x in y:
-            assert isinstance(x, int)
-
-    return grid
-
-
 if __name__ == '__main__':
     data = get_line_data("08")
-    grid = parse_data(data)
+    grid = Grid([list(map(int, row)) for row in data])
 
     p1_result = grid.visible_trees()
     print(p1_result)
