b26d55b53dae6516cfc738fcf58085ec8b55742a
Caleb Courtney <caleb-courtney@pluralsight.com>
2022-12-12 08:39:49 -0800

    pre-compute distance between all points and the end

--- 2022/python/day12.py
+++ 2022/python/day12.py
@@ -3,45 +3,46 @@ from utils import get_line_data, Point
 from collections import deque
 
 
-def parse_lines(lines):
-    board = {}
-    for y, line in enumerate(lines):
-        for x, c in enumerate(line.strip()):
-            board[Point(x, y)] = c
-            if c == 'S':
-                start = Point(x, y)
-                board[start] = 'a'  # min height, but still lowercase
-            elif c == 'E':
-                end = Point(x, y)
-                board[end] = 'z'  # max height, but still lowercase
+class Grid:
+    def __init__(self, lines):
+        self.board = {}
+        for y, line in enumerate(lines):
+            for x, c in enumerate(line.strip()):
+                if c == 'S':
+                    self.end = Point(x, y)
+                    self.board[self.end] = 'a'
+                elif c == 'E':
+                    self.start = Point(x, y)
+                    self.board[self.start] = 'z'
+                else:
+                    self.board[Point(x, y)] = c
 
-    return board, start, end
+        self.steps = {}
+        self.compute_shortest_paths(end=self.end)
 
+    def compute_shortest_paths(self, end=None):
+        queue = deque([(self.start, 0)])
+        while queue:
+            current, depth = queue.popleft()
+            for n in current.neighbors():
+                if n not in self.board:
+                    continue
 
-def solve(data, start, end):
-    queue = deque([(start, 0)])
-    seen = set()
-    while queue:
-        current, depth = queue.popleft()
-        if current in seen:
-            continue
-        elif current == end:
-            return depth
+                if n in self.steps:
+                    continue
 
-        seen.add(current)
+                if (ord(self.board[n]) + 1 >= ord(self.board[current])):
+                    self.steps[n] = depth + 1
+                    queue.append((n, depth + 1))
 
-        for n in current.neighbors():  # returns the 4 adjacent points
-            if n not in board:
-                continue
-            if (ord(board[n]) - 1 <= ord(board[current])):
-                queue.append((n, depth + 1))
-
-    return 1e50
+        for point in self.board.keys():
+            if point not in self.steps:
+                self.steps[point] = float('inf')
 
 
 if __name__ == '__main__':
     lines = get_line_data("12")
-    board, start, end = parse_lines(lines)
+    grid = Grid(lines)
 
-    print(solve(board, start, end))
-    print(min(solve(board, s, end) for s in board if board[s] == 'a'))
+    print(grid.steps[grid.end])
+    print(min(grid.steps[point] for point, value in grid.board.items() if value == "a"))
