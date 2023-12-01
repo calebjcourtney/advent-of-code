cd6d2ecefe9c575b1f44c1b18349544c4c390027
Caleb Courtney <brokenfoot@rocketmail.com>
2023-12-01 08:54:39 -0800

    Use a simple sort instead of my garbage min/max nonsense.

--- 2023/python/day01.py
+++ 2023/python/day01.py
@@ -10,17 +10,20 @@ def part_one(lines: list[str]):
 NUMS = "0 zero 1 one 2 two 3 three 4 four 5 five 6 six 7 seven 8 eight 9 nine".split()
 
 
-def indexes(line: str) -> list[tuple[str, int]]:
-    return [(x, line.index(x)) for x in NUMS if x in line]
+def indices(line: str) -> list[tuple[str, int]]:
+    idx = [(x, line.index(x)) for x in NUMS if x in line]
+    idx.sort(key=lambda x: x[1])
+
+    return idx
 
 
 def part_two(lines: list[str]):
     answer = 0
 
     for line in lines:
-        idx = indexes(line)
-        first = min(idx, key=lambda x: x[1])[0]
-        last = max(idx, key=lambda x: x[1])[0]
+        idx = indices(line)
+        first = idx[0][0]
+        last = idx[-1][0]
         answer += int(f"{NUMS.index(first) // 2}{NUMS.index(last) // 2}")
 
     return answer
