a2d3803e301971d9566531f0b7eeac4afd37ab2f
Caleb Courtney <brokenfoot@rocketmail.com>
2023-12-05 21:43:27 -0800

    use a quadratic formula - much faster

--- 2023/python/day06.py
+++ 2023/python/day06.py
@@ -1,33 +1,39 @@
+from math import prod
+
 from utils import get_line_data
 
 
 def solve(time, record):
-    num_wins = 0
-    for hold_time in range(time + 1):
-        total_distance = hold_time * (time - hold_time)
-        if total_distance > record:
-            num_wins += 1
+    # quadratic
+    upper_limit = (time + (time ** 2 - 4 * record) ** 0.5) // 2
+    lower_limit = (time - (time ** 2 - 4 * record) ** 0.5) // 2
 
-    return num_wins
+    # handle the rounding issues
+    for x in range(int(lower_limit) - 2, int(lower_limit) + 2):
+        if x * (time - x) > record:
+            lb = x
+            break
+
+    for x in range(int(upper_limit) - 2, int(upper_limit) + 2):
+        if x * (time - x) < record:
+            ub = x
+            break
+
+    return ub - lb
 
 
 def part_one(data):
     times = list(map(int, data[0].split(":")[1].split()))
-    distances = list(map(int, data[1].split(":")[1].split()))
+    records = list(map(int, data[1].split(":")[1].split()))
 
-    output = 1
-
-    for time, dist in zip(times, distances):
-        output *= solve(time, dist)
-
-    return output
+    return prod(solve(t, d) for t, d in zip(times, records))
 
 
 def part_two(data):
     time = int("".join(data[0].split(":")[1].strip().split()))
-    distance = int("".join(data[1].split(":")[1].strip().split()))
+    record = int("".join(data[1].split(":")[1].strip().split()))
 
-    return solve(time, distance)
+    return solve(time, record)
 
 
 if __name__ == '__main__':
