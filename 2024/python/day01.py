b0e97ae4bca8bb6c54e0b2481906e9b8ee54eab5
Caleb Courtney <caleb-courtney@pluralsight.com>
2024-12-01 20:52:20 -0800

    simplify further

--- 2024/python/day01.py
+++ 2024/python/day01.py
@@ -1,30 +1,17 @@
-from collections import Counter
-
 from utils import get_line_data
 from utils import get_nums
 from utils import timeit
 
 
 @timeit
-def part_one(data: tuple[str, str]) -> int:
+def combined(data: tuple[str, str]) -> None:
     lefts = sorted(list(map(int, [x[0] for x in data])))
     rights = sorted(list(map(int, [x[1] for x in data])))
 
-    return sum([abs(x - y) for x, y in zip(lefts, rights)])
-
-
-@timeit
-def part_two(data: tuple[str, str]):
-    lefts = Counter([x[0] for x in data])
-    rights = Counter([x[1] for x in data])
-
-    return sum([
-        int(x) * lefts[x] * rights[x]
-        for x in set(lefts.keys()) | set(rights.keys())
-    ])
+    print("part 1:", sum([abs(x - y) for x, y in zip(lefts, rights)]))
+    print("part 2:", sum(l * rights.count(l) for l in lefts))
 
 
 if __name__ == '__main__':
     data = [get_nums(line) for line in get_line_data("01")]
-    print(part_one(data))
-    print(part_two(data))
+    combined(data)
