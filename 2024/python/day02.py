ee637923876778c5d610116ae1f89f03b2d122ac
Caleb Courtney <caleb-courtney@pluralsight.com>
2024-12-02 07:05:06 -0800

    clean up the code

--- 2024/python/day02.py
+++ 2024/python/day02.py
@@ -2,41 +2,30 @@ from utils import get_line_data
 from utils import get_nums
 
 
-def is_order_correct(record: list[int]) -> bool:
-    return any([
-        sorted(record) == record,
-        sorted(record, reverse=True) == record,
-    ])
-
-
-def numbers_gap(record: list[int]):
-    for a, b in zip(record, record[1:]):
-        if not (1 <= abs(b - a) <= 3):
-            return False
-
-    return True
-
-def part_one(record):
+def is_safe(record) -> bool:
     return all([
-        is_order_correct(record),
-        numbers_gap(record),
+        # is ascending or descending
+        any([
+            sorted(record) == record,
+            sorted(record, reverse=True) == record,
+        ]),
+        all([
+            1 <= abs(b - a) <= 3
+            for a, b in zip(record, record[1:])
+        ]),
     ])
 
 
-def part_two(record):
-    if part_one(record):
-        return True
-
-    for i in range(len(record)):
-        if part_one(record[:i] + record[i+1:]):
-            return True
-
-    return False
+def part_two(record) -> bool:
+    return any([
+        is_safe(record[:i] + record[i + 1:])
+        for i in range(len(record))
+    ])
 
 
 if __name__ == '__main__':
     data = get_line_data("02")
     data = [list(map(int, get_nums(line))) for line in data]
 
-    print(sum(part_one(record) for record in data))
-    print(sum(part_two(record) for record in data))
+    print("part 1:", sum(is_safe(record) for record in data))
+    print("part 2:", sum(part_two(record) for record in data))
