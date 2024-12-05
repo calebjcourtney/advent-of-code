42c0105e92b71e95c9e6a35d76d19f163a9391a6
Caleb Courtney <caleb-courtney@pluralsight.com>
2024-12-04 21:51:41 -0800

    return list of broken rules instead of just valid/invalid

--- 2024/python/day05.py
+++ 2024/python/day05.py
@@ -10,40 +10,41 @@ def parse_data(data: str) -> list[list[int]]:
     ]
 
 
-def record_is_valid(record: list[int], rules: list[list[int]]) -> bool:
+def broken_rules(record: list[int], rules: list[list[int]]) -> tuple[bool, list[list[int]] | None]:
+    broken_rules = []
     for rule in rules:
         if (
             set(rule) & set(record) == set(rule) and record.index(rule[0]) > record.index(rule[1])
         ):
-            return False, rule
+            broken_rules.append(rule)
 
-    return True, None
+    return broken_rules
 
 
 @timeit
 def part_one(pages, rules):
-    return sum([
+    return sum(
         line[len(line) // 2]
         for line in pages
-    ])
+    )
 
 
 @timeit
 def part_two(pages, rules):
     output = 0
     for line in pages:
-        valid, broken_rule = record_is_valid(line, rules)
-        while not valid:
-            add_record = True
-            (
-                line[line.index(broken_rule[0])],
-                line[line.index(broken_rule[1])],
-            ) = (
-                line[line.index(broken_rule[1])],
-                line[line.index(broken_rule[0])],
-            )
+        broken = broken_rules(line, rules)
+        while broken:
+            for broken_rule in broken:
+                (
+                    line[line.index(broken_rule[0])],
+                    line[line.index(broken_rule[1])],
+                ) = (
+                    line[line.index(broken_rule[1])],
+                    line[line.index(broken_rule[0])],
+                )
 
-            valid, broken_rule = record_is_valid(line, rules)
+            broken = broken_rules(line, rules)
 
         output += line[len(line) // 2]
 
@@ -55,10 +56,10 @@ def main(pages, rules):
     valid_pages = []
     invalid_pages = []
     for record in pages:
-        if record_is_valid(record, rules)[0]:
-            valid_pages.append(record)
-        else:
+        if broken_rules(record, rules):
             invalid_pages.append(record)
+        else:
+            valid_pages.append(record)
 
     print(part_one(valid_pages, rules))
     print(part_two(invalid_pages, rules))
@@ -71,5 +72,3 @@ if __name__ == '__main__':
     pages = parse_data(pages)
 
     main(pages, rules)
-
-
