da4d7a1c771d56427b4a41f0c73921e912ee4982
Caleb Courtney <caleb-courtney@pluralsight.com>
2024-12-10 22:10:27 -0800

    slight speed increase by removing the list append

--- 2024/python/day11.py
+++ 2024/python/day11.py
@@ -6,16 +6,12 @@ from collections import Counter
 
 
 def get_new(num: int):
-    output = []
     if num == 0:
-        output.append(1)
+        return 1, None
     elif len(str(num)) % 2 == 0:
-        output.append(int(str(num)[:len(str(num))//2]))
-        output.append(int(str(num)[len(str(num))//2:]))
+        return int(str(num)[:len(str(num))//2]), int(str(num)[len(str(num))//2:])
     else:
-        output.append(num * 2024)
-
-    return output
+        return num * 2024, None
 
 
 @timeit
@@ -23,9 +19,10 @@ def solve(counter, cycles):
     for _ in range(cycles):
         tmp = Counter()
         for key, value in counter.items():
-            new_nums = get_new(key)
-            for n in new_nums:
-                tmp[n] += value
+            f, s = get_new(key)
+            tmp[f] += value
+            if s:
+                tmp[s] += value
 
         counter = tmp
 
