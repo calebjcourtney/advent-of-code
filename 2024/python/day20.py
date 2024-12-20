aff77211d438023f5e3bc93224a0f89d3cbd7174
Caleb Courtney <caleb-courtney@pluralsight.com>
2024-12-20 11:17:31 -0800

    add code for day 20

---
+++ 2024/python/day20.py
@@ -0,0 +1,47 @@
+from utils import get_data
+from utils import parse_grid
+from utils import timeit
+from utils import DIRS
+from utils import manhattan
+
+from collections import deque
+from itertools import combinations
+
+
+def get_distances(grid, start):
+    distances = {start: 0}
+    queue = deque([start])
+
+    while queue:
+        pos = queue.popleft()
+        for direction in DIRS:
+            new = pos + direction
+            if new in grid and new not in distances:
+                distances[new] = distances[pos] + 1
+                queue.append(new)
+
+    return distances
+
+@timeit
+def main(grid):
+    start = [p for p in grid if grid[p] == 'S'][0]
+    distances = get_distances(grid, start)
+
+    p1 = 0
+    p2 = 0
+
+    for (p, p_dist), (q, q_dist) in combinations(distances.items(), 2):
+        dist = manhattan(p, q)
+        p1 += (dist == 2 and q_dist - p_dist - dist >= 100)
+        p2 += (dist < 21 and q_dist - p_dist - dist >= 100)
+
+    print(p1)
+    print(p2)
+
+
+if __name__ == '__main__':
+    grid = parse_grid(
+        get_data("20"),
+        keep_values='.SE',
+    )
+    main(grid)
