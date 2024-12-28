4d5a0700289505343e29ae75d883a4d557186150
Caleb Courtney <caleb-courtney@pluralsight.com>
2024-12-27 19:13:47 -0800

    use set inclusion like Peter

--- 2024/python/day25.py
+++ 2024/python/day25.py
@@ -1,50 +1,25 @@
-from utils import get_data
-from utils import parse_grid
-from utils import timeit
-from utils import Point
+from utils import get_data, parse_grid
 
-from collections import Counter
 
 def parse_data(data):
     records = data.split("\n\n")
 
-    keys: list[Counter] = []
-    locks: list[Counter] = []
+    keys = []
+    locks = []
 
     for record in records:
         grid = parse_grid(record, keep_values="#")
 
-        heights = Counter(point.x for point in grid.keys())
-
-        if Point(0, 0) in grid:
-            keys.append(heights)
+        if (0, 0) in grid:
+            keys.append(set(grid.keys()))
         else:
-            locks.append(heights)
+            locks.append(set(grid.keys()))
 
     return keys, locks
 
 
-def no_overlap(key, lock):
-    for x in range(6):
-        if key[x] + lock[x] > 7:
-            return False
-
-    return True
-
-
-@timeit
-def part_one(keys, locks):
-    output = 0
-    for key in keys:
-        for lock in locks:
-            output += no_overlap(key, lock)
-
-    return output
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     data = get_data("25")
     keys, locks = parse_data(data)
 
-    p1_result = part_one(keys, locks)
-    print(p1_result)
+    print(sum(1 for key in keys for lock in locks if len(key & lock) == 0))
