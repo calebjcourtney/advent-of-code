7df17688b65e021797fc59e071d84018b22ddba2
Caleb Courtney <caleb+c@populi.co>
2025-12-03 21:31:15 -0800

    minor enhance

--- 2025/python/day04.py
+++ 2025/python/day04.py
@@ -5,7 +5,7 @@ from utils import DIRS_8
 
 
 def count_neighbors(grid, point):
-    return sum(1 for direction in DIRS_8 if grid.get(point + direction, ".") != ".")
+    return sum(1 for direction in DIRS_8 if grid.get(point + direction) is not None)
 
 
 @timeit
@@ -22,22 +22,18 @@ def part_one(grid):
 @timeit
 def part_two(grid):
     total_removed = 0
+    changed = True
 
-    while True:
-        points_to_remove = []
-
+    while changed:
+        changed = False
         for point in grid:
             neighbor_count = count_neighbors(grid, point)
             if neighbor_count < 4:
-                points_to_remove.append(point)
-                grid[point] = "."
+                grid[point] = None
+                total_removed += 1
+                changed = True
 
-        if not points_to_remove:
-            break
-
-        for point in points_to_remove:
-            del grid[point]
-            total_removed += 1
+        grid = {key: value for key, value in grid.items() if value}
 
     return total_removed
 
@@ -45,8 +41,7 @@ def part_two(grid):
 @timeit
 def main():
     data = get_data("04")
-    grid = parse_grid(data)
-    grid = {key: value for key, value in grid.items() if value == "@"}
+    grid = parse_grid(data, keep_values="@")
 
     p1_result = part_one(grid)
     print(p1_result)
