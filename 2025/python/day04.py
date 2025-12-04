d127e5d5dbd5a6c01fdf1d50c8d33c4e3e70d80e
Caleb Courtney <caleb+c@populi.co>
2025-12-03 21:42:27 -0800

    make the grid a dict[point[int]]

--- 2025/python/day04.py
+++ 2025/python/day04.py
@@ -10,38 +10,31 @@ def count_neighbors(grid, point):
 
 @timeit
 def part_one(grid):
-    total = 0
-    for point in grid:
-        neighbor_count = count_neighbors(grid, point)
-        if neighbor_count < 4:
-            total += 1
-
-    return total
+    return sum(1 for point in grid if grid[point] < 4)
 
 
 @timeit
 def part_two(grid):
     total_removed = 0
-    changed = True
+    prev_grid = grid
+    while True:
+        points = [key for key in grid if grid[key] < 4]
+        if len(points) == 0:
+            return total_removed
 
-    while changed:
-        changed = False
-        for point in grid:
-            neighbor_count = count_neighbors(grid, point)
-            if neighbor_count < 4:
-                grid[point] = None
-                total_removed += 1
-                changed = True
-
-        grid = {key: value for key, value in grid.items() if value}
-
-    return total_removed
+        total_removed += len(points)
+        for point in points:
+            del grid[point]
+            for neighbor in DIRS_8:
+                if grid.get(point + neighbor) is not None:
+                    grid[point + neighbor] -= 1
 
 
 @timeit
 def main():
     data = get_data("04")
     grid = parse_grid(data, keep_values="@")
+    grid = {key: count_neighbors(grid, key) for key in grid}
 
     p1_result = part_one(grid)
     print(p1_result)
